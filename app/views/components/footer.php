c936a17b5077774f2c1e18d63a87c81bf4b646d8
IF-B35 <rranggadwi1@gmail.com>
2023-08-04 18:10:17 +0700

    Header dan Footer

---
+++ app/views/components/footer.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+
+    <footer class="bg-dark text-white text-center py-3">
+        <div class="container">
+            <p>JKL-CROWDFUNDING</p>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
