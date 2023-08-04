955ad703f8262bc0565c0ff758058187a15bdf1d
zxyn23 <ajay.fadillah22@gmail.com>
2023-08-04 19:16:20 +0700

    Halaman Home

---
+++ app/views/home/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Utama</title>
+
+    <!-- Tautan ke Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-8">
+                <!-- Tambahkan tag gambar (image) sebelum div "Selamat Datang" -->
+                <img src="img/donasi.jpg" alt="Gambar Donasi" class="img-fluid">
+
+                <h1>Selamat datang di Halaman Utama</h1>
+                <p>Ini adalah halaman utama website Anda. Anda dapat menambahkan konten utama di sini.</p>
+
+                <!-- Tambahkan tombol "Daftar" yang mengarahkan ke view "register" -->
+                <a href="register.php" class="btn btn-primary">Daftar</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Skrip JavaScript Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
