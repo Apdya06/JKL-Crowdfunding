ab3dbaaf06b7cf66cfab281a76c976af4fd33f71
IF-B35 <rranggadwi1@gmail.com>
2023-08-05 16:18:24 +0700

    Update Halaman Register

--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,36 +1,54 @@
 <html>
 <head>
-    <title>Halaman Register TEMP</title>
+    <title>Halaman Register</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
-    <div class="container mt-5">
+    <div class="wrapper container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">Login</div>
+                    <div class="card-header">
+                    <h4 class="mb-0">Registrasi Akun</h4>
+                    </div>
                     <div class="card-body">
-                        <form action="process_login.php" method="post">
-                            <div class="form-group">
-                                <label for="username">Username</label>
-                                <input type="text" name="username" id="username" class="form-control" required>
+                        <form method="post">
+                            <div class="form-group mb-3">
+                                <label for="username">Username:</label>
+                                <input type="text" id="username" name="username" class="form-control" required>
                             </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" class="form-control" required>
+                            <div class="form-group mb-3">
+                                <label for="email">Email:</label>
+                                <input type="email" id="email" name="email" class="form-control" required>
                             </div>
-                            <br>
-                            <button type="submit" class="btn btn-primary">Login</button>
+
+                            <div class="form-group mb-3">
+                                <label for="password">Password:</label>
+                                <input type="password" id="password" name="password" class="form-control" required>
+                            </div>
+
+                            <div class="form-group mb-3">
+                                <label for="user_type">User Type:</label>
+                                <select id="user_type" name="user_type" class="form-control">
+                                    <option value="" disabled selected hidden></option>
+                                    <option value="Personal">Personal</option>
+                                    <option value="Business">Business</option>
+                                    <option value="Charities">Charities</option>
+                                </select>
+                            </div>
+
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">Register</button>
+                            </div>
                         </form>
-                        <p>Sudah punya akun?</p><a href="./login">Daftar Disini!</a>
+                        <p>Sudah punya akun?</p><a href="./login">Masuk Disini!</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
